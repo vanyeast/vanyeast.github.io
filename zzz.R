941d01727caf20085e19f6c785359276f150a892
npjc <nicolas.coutin@gmail.com>
2015-09-29 15:04:19 -0700

    rebase into root of master

--- zzz.R
+++ zzz.R
@@ -1,16 +1,18 @@
 system("gitbook init .")
 system("gitbook build")
+# copy all rendered files from default output location to base of root dir
+system("cp -R ./_book/* ./")
 
 # from jbryer/Rgitbook internals
-repo <- "yeastmeeting-vancouver/yeastmeeting-vancouver.github.io"
-dest <- file.path(getwd(), "_book")
-message <- "push gitbook website"
-cmd <- paste0(
-  "cd ", dest, " \n",
-  "git init \n",
-  "git commit --allow-empty -m '", message,"' \n",
-  "git checkout -b gh-pages \n",
-  "git add . \n",
-  "git commit -am '", message, "' \n",
-  "git push git@github.com:", repo, " gh-pages --force ")
-system(cmd)
+# repo <- "yeastmeeting-vancouver/yeastmeeting-vancouver.github.io"
+# dest <- file.path(getwd(), "_book")
+# message <- "push gitbook website"
+# cmd <- paste0(
+#   "cd ", dest, " \n",
+#   "git init \n",
+#   "git commit --allow-empty -m '", message,"' \n",
+#   "git checkout -b gh-pages \n",
+#   "git add . \n",
+#   "git commit -am '", message, "' \n",
+#   "git push git@github.com:", repo, " gh-pages --force ")
+# system(cmd)
